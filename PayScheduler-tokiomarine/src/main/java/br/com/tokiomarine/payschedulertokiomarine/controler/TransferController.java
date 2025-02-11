541953ee17569ff60f2e5082f47cdd426d45b191
Gabriel Silva Ezequiel <77858768+Gabrie345@users.noreply.github.com>
2025-02-11 15:53:30 -0300

    DTO validation, controller

--- PayScheduler-tokiomarine/src/main/java/br/com/tokiomarine/payschedulertokiomarine/controler/TransferController.java
+++ PayScheduler-tokiomarine/src/main/java/br/com/tokiomarine/payschedulertokiomarine/controler/TransferController.java
@@ -1,11 +1,32 @@
 package br.com.tokiomarine.payschedulertokiomarine.controler;
 
 
+import br.com.tokiomarine.payschedulertokiomarine.dto.TransferDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="transferencia")
 public class TransferController {
 
+    @PostMapping
+    public ResponseEntity<TransferDto> transfer (@Valid @RequestBody TransferDto transfer){
+
+        return ResponseEntity.ok().body(transfer);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TransferDto>> listTransfers (String account){
+
+        return null;
+    }
 }
---
+++ PayScheduler-tokiomarine/src/main/java/br/com/tokiomarine/payschedulertokiomarine/dto/TransferDto.java
@@ -0,0 +1,65 @@
+package br.com.tokiomarine.payschedulertokiomarine.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TransferDto {
+
+    @JsonProperty("conta_origem")
+    @NotNull(message = "a conta origem é obrigatório")
+    @Size(min = 10, max = 10, message = "A conta origem deve ter exatamente 10 caracteres")
+    private String OriginAccount;
+
+    @NotNull(message = "a conta destino é obrigatório ")
+    @JsonProperty("conta_destino")
+    @Size(min = 10, max = 10, message = "A conta origem deve ter exatamente 10 caracteres")
+    private String destinationAccount;
+
+    @JsonProperty("valor")
+    @NotNull(message = "o valor é obrigatório")
+    private double value;
+
+    @JsonProperty("data_Agendamento")
+    @NotNull(message = "a data de agendamento é obrigatório")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate appointmentDate;
+
+
+    public String getOriginAccount() {
+        return OriginAccount;
+    }
+
+    public void setOriginAccount(String originAccount) {
+        OriginAccount = originAccount;
+    }
+
+    public String getDestinationAccount() {
+        return destinationAccount;
+    }
+
+    public void setDestinationAccount(String destinationAccount) {
+        this.destinationAccount = destinationAccount;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+}
