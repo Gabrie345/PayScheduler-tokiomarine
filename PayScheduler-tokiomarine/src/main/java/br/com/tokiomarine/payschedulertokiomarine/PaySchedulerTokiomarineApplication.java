3109865842f8ca8a0254d8edfaa6520dced84f59
Gabriel Silva Ezequiel <77858768+Gabrie345@users.noreply.github.com>
2025-02-11 11:55:04 -0300

    Git init

---
+++ PayScheduler-tokiomarine/src/main/java/br/com/tokiomarine/payschedulertokiomarine/PaySchedulerTokiomarineApplication.java
@@ -0,0 +1,13 @@
+package br.com.tokiomarine.payschedulertokiomarine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySchedulerTokiomarineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaySchedulerTokiomarineApplication.class, args);
+	}
+
+}
